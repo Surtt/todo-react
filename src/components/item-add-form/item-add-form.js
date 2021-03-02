cc51fba1339b7569549289e2b514bbeb310d717a
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2021-03-02 21:23:48 +0200

    search added

---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import './item-add-form.css';
+
+export default class ItemAddForm extends Component {
+
+  state = {
+    label: '',
+  };
+
+  onLabelChange = (e) => {
+    this.setState({ label: e.target.value });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState({ label: '' });
+  };
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} className="item-add-form d-flex">
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done"
+          value={this.state.label} />
+        <button
+          className="btn btn-outline-secondary">
+          Add Item
+          </button>
+      </form>
+    );
+  }
+}
---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import './todo-list-item.css';
+
+export default class TodoListItem extends Component {
+  render() {
+    const { label, onDeleted, onToggleImportant, onToggleDone, important, done } = this.props;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+
+    if (important) {
+      classNames += ' important';
+    }
+
+    return (
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          onClick={onToggleDone}>
+          {label}
+        </span>
+
+        <button onClick={onToggleImportant} type="button"
+          className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button onClick={onDeleted} type="button"
+          className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  }
+}
